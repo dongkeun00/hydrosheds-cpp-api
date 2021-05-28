310221bc935a7a5146f47e0e977a386ca7a66316
dongkeun00 <dongkeun2000@gmail.com>
2021-05-28 02:46:14 +0200

    task 28.05.21

--- app/hydrosheds_app.cc
+++ app/hydrosheds_app.cc
@@ -1,134 +1,6 @@
 #include "hydrosheds/hydrosheds.hh"
 
-#include "ogrsf_frmts.h"
-
 #include<iostream>
-
-
-using namespaces hydrosheds;
-
-//HadroshedsDataSet
-
-//HydroshedsData constructor
-HydroshedsDataSet::HydroshedsDataSet(char* s)
-{
-  GDALAllRegister();
-
-layer = poDS->GetLayerByName(s);
-
-}
-
-//give Data to RiverSegment
-RiverSegment HydroshedsDataSet::getSegment() const
-{
-
-}
-
-
-
-//RiverSegment
-//RiverSegment constructor
-RiverSegment::RiverSegment(OGRFeature *b, unsigned int c)
-{
-  this->c;
-  auto feature = p->getFeature();
-  OGRLayer *layer;
-  feature=layer->getFeature(c);
-}
-
-
-//get Length
-double RiverSegment::getLength() const
-{
-  virtual void segmentize(double dfMaxLength)
-  {
-    //calculate value between start and end point dont really know how to if points are missing 
-    //dfMaxLength = 
-  }
-}
-
-//get geological Length
-double RiverSegment::getGeologicalLength() const
-{
-  //maybe we can use a scale and use the same method like in get length?
-}
-
-//get Discharge
-double RiverSegment::getDischarge() const
-{
-
-}
-
-
-
-//get starting Point of RiverSegment
-Coordinate RiverSegment::getStartingPoint() const
-{
-  //set Dimension to 2
-  virtual void setCoordinateDimension(int nDimension) override{
-    int nDimension=2;
-    //get geometric attributes
-    OGRGeometry *pGeo = feature->getGeometryRef;
-    //fetch geometry from container
-    OGRGeometry *getGeometryRef(int i);
-    //reference for point
-    OGRPoint *point = &pGeo;
-
-    Coordinate segEnd;
-    //get coordinates, not sure if this actually works
-    segStart[0] = point -> getX();
-    segStart[1] = point -> getY();
-
-
-}
-
-//get ending Point of RiverSegment
-Coordinate RiverSegment::getEndPoint()
-{
-  //set Dimension to 2
-  virtual void setCoordinateDimension(int nDimension) override{
-    int nDimension=2;
-    //get geometric attributes
-    OGRGeometry *pGeo = feature->getGeometryRef;
-    //fetch geometry from container
-    OGRGeometry *getGeometryRef(int i);
-    //reference for point
-    OGRPoint *point = &pGeo;
-
-    Coordinate segEnd;
-    //get coordinates, not sure if this actually works
-    //dont really know how to get the endpoint if the code is for the start
-    segEnd[0] = point -> getX();
-    segEnd[1] = point -> getY();
-
-  }
-  
-
-}
-
-
-//show if Riversegment has Downstream
-bool RiverSegment::hasDownstreamSegment() const
-{
-  //Idee: Falls Endpunkt von RiverSegment != Startpunkt von NextRiverSegment return false, else true
-  /*auto *poDS = feature-> getNextRiverSegment();
-
-  if(segEnd!=getNextRiverSegment()){
-    return false;
-  }
-  else 
-    return true;
-*/
-}
-
-//get the Down Stream Segment if RiverSegment has one
-double RiverSegment::getDownstreamSegment() const
-{
-
-}
-
-//
-
 
 
 int main(int argc, char** argv)
@@ -138,27 +10,21 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  GDALAllRegister();
+  // Instantiate the data set
+  hydrosheds::HydroshedsDataSet dataset(argv[1]);
 
-  GDALDataset* data;
-  data = (GDALDataset*) GDALOpenEx(argv[1], GDAL_OF_VECTOR, NULL, NULL, NULL );
+  // Printing some example data
+  auto segment = dataset.getSegment();
+  auto start = segment.getDownstreamSegment().getStartingPoint();
+  auto end = segment.getDownstreamSegment().getEndPoint();
+  std::cout << end[0] << " " << end[1] << std::endl;
+  std::cout << start[0] << " " << start[1] << std::endl;
 
-  if(data == NULL)
-  {
-      std::cerr << "Opening geodatabase failed." << std::endl;
-      return 1;
-  }
+  auto length = segment.getLength();
+  std::cout<<length<<std::endl;
 
-  std::cout << "Layer count in GeoDataBase: " << data->GetLayerCount() << std::endl;
-  std::cout << "Test" << std::endl;
+  auto geolength = segment.getGeologicalLength();
+  std::cout<<geolength<<std::endl;
 
-
-  // Extract the first layer
-  auto layer = data->GetLayer(0);
-  
-  // Extract first feature
-  auto feature = layer->GetNextFeature();
-  feature->DumpReadable(NULL, NULL);
-  
   return 0;
 }
--- src/hydrosheds.cc
+++ src/hydrosheds.cc
@@ -1,5 +1,118 @@
 #include "hydrosheds/hydrosheds.hh"
 
+#include "ogrsf_frmts.h"
+
+#include<iostream>
+#include<string>
+#include<math.h>
+
 namespace hydrosheds {
+
+  HydroshedsDataSet::HydroshedsDataSet(const std::string& path)
+  {
+    GDALAllRegister();
+    auto data = (GDALDataset*) GDALOpenEx(path.c_str(), GDAL_OF_VECTOR, NULL, NULL, NULL);
+
+    if(data == NULL)
+    {
+      std::cerr << "Opening geodatabase failed." << std::endl;
+      throw std::exception{};
+    }
+
+    layer = data->GetLayer(0);
+  }
+
+  RiverSegment HydroshedsDataSet::getSegment() const
+  {
+    // This is extracting just some random feature for debugging purposes.
+    // When we move on to iterators, this will vanish.
+    return RiverSegment(layer, layer->GetFeature(89));
+  }
+
+  RiverSegment::RiverSegment(OGRLayer* layer, OGRFeature* feature)
+    : layer(layer)
+    , feature(feature)
+  {}
+
+  RiverSegment RiverSegment::getDownstreamSegment() const
+  {
+    // The modulo operation here is weird, but necessary if the GetFeature(index)
+    // method is supposed to be used: It removes the continent encoding prefix
+    auto next_index = feature->GetFieldAsInteger("NEXT_DOWN") % 10000000;
+    return RiverSegment(layer, layer->GetFeature(next_index));
+  }
+
+  double RiverSegment::getDischarge() const
+  {
+    return feature->GetFieldAsDouble("DIS_AV_CMS");
+  }
+
+  Coordinate RiverSegment::getStartingPoint() const
+  {
+    // Get the most specific GDAL object that we can find that represents our curve
+    OGRGeometry* geo = feature->GetGeometryRef();
+    OGRMultiLineString* multiline = geo->toMultiLineString();
+    OGRLineString* line = *(multiline->begin());
+
+    // Extract the curve start point
+    OGRPoint start;
+    line->StartPoint(&start);
+
+    return Coordinate{start.getX(), start.getY()};
+  }
+
+  Coordinate RiverSegment::getEndPoint() const
+  {
+    // TODO: TASK
+    OGRGeometry* geo = feature->GetGeometryRef();
+    OGRMultiLineString* multiline = geo->toMultiLineString();
+    OGRLineString* line = *(multiline->begin());
+
+    OGRPoint start;
+    line->EndPoint(&start);
+
+     return Coordinate{start.getX(), start.getY()};
+  }
+//had no idea how to implement this one
+  /*bool RiverSegment::hasDownstreamSegment() const
+  {
+    // TODO: TASK
+    OGRGeometry* down = feature->GetGeometryRef();
+    OGRMultiLineString* multiline = geo->toMultiLineString();
+    OGRLineString* line = *(multiline->begin());
+    
+    if( )
+
+    return 0;
+  }*/
+
+  double RiverSegment::getLength() const
+  {
+    // TODO: TASK
+    OGRGeometry* geo = feature->GetGeometryRef();
+    OGRMultiLineString* multiline = geo->toMultiLineString();
+    OGRLineString* line = *(multiline->begin());
+    OGRPoint start;
+    line->StartPoint(&start);
+    OGRPoint end;
+    line->EndPoint(&end);
+
+    auto x = pow(end.getX()-start.getX(),2);
+    auto y = pow(end.getY()-start.getY(),2);
+    auto length = sqrt(x+y);
+    return length;
+    }
+
+  double RiverSegment::getGeologicalLength() const
+  {
+    // TODO: TASK
+    OGRGeometry* geo = feature->GetGeometryRef();
+    OGRMultiLineString* multiline = geo->toMultiLineString();
+    OGRLineString* line = *(multiline->begin());
+    OGRPoint start;
+
+    auto geolength = line->get_Length();
+    return geolength;  
+    }
 
 } // namespace hydrosheds
